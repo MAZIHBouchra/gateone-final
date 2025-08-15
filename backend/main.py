c167f0e0ff1a29c6f4ac5510b19b4df14d4a175a
gateoneinfo-beep <gateoneinfo@gmail.com>
2025-08-15 23:39:27 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -31,6 +31,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:8080",
         "http://127.0.0.1:8080",
+        "https://appealing-smile-production.up.railway.app/"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "DELETE", "OPTIONS"],
@@ -86,3 +87,4 @@ async def health_check():
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
